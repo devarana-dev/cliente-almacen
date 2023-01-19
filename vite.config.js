49a174a1df32b34fe2738307c128a6271b7df085
Abraham Alvarado <abrahamalvarado@devarana.mx>
2023-01-19 12:24:52 -0600

    Ajuste Vite 2

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,35 @@ export default () => {
     server: {
         port: 3000,
     },
-    // rename the output file
     build: {
         outDir: "build",
         chunkSizeWarningLimit: 10000,
+        // Optimization
+        brotliSize: false,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    "react-dom": ["react-dom"],
+                    "react": ["react"],
+                    "react-router-dom": ["react-router-dom"],
+                    "react-router": ["react-router"],
+                    "react-redux": ["react-redux"],
+                    "redux": ["redux"],
+                    "redux-thunk": ["redux-thunk"],
+                    "axios": ["axios"],
+                    "moment": ["moment"],
+                    "antd": ["antd"],
+                    "chart.js": ["chart.js"],
+                    "react-chartjs-2": ["react-chartjs-2"],
+                    "nanoid": ["nanoid"],
+                    "animate.css": ["animate.css"],
+                    "react-dropzone": ["react-dropzone"],
+                    "react-icons": ["react-icons"],
+                    "icons": ["@ant-design/icons"],
+                },
+            },
+        },
+
     }
   })
 }
