bcff5e427f6a26b11b51c6401fcaa6ffe5c37bb1
AbrahamDevarana <99768357+AbrahamDevarana@users.noreply.github.com>
2023-02-28 09:34:53 -0600

    Ajuste Menú

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -33,36 +33,40 @@ export default function FooterLayout({showModal, userPermission}) {
                     : null
                 }
                 
-                <Link to={'/bitacora'} className="block w-full h-auto text-center footer__link">
-                    <div className="footer__link-icon">
-                        <ProfileOutlined className="m-auto"/>
-                        <p> Bitácora </p>
-                    </div>
-                </Link>
+                {
+                    hasPermission(userPermission, 'crear bitacora') ?
+                    <Link to={'/bitacora'} className="block w-full h-auto text-center footer__link">
+                        <div className="footer__link-icon">
+                            <ProfileOutlined className="m-auto"/>
+                            <p> Bitácora </p>
+                        </div>
+                    </Link>
+                    : null
+                }
                {
                     groupPermission(userPermission, ['crear vales', 'crear personal']) ?
                     <div className="block w-full h-auto text-center footer__link cursor-pointer">
                     <Dropdown 
                         menu={{
                             items : [
-                                    {
-                                        key: '2',
-                                        label: 'Prestamos',
-                                        icon: <SwapOutlined />,
-                                        onClick: () => navigate('/prestamos')
-                                    },
-                                    {
-                                        key: '1',
-                                        label: 'Personal',
-                                        icon: <AiOutlineUserAdd/>,
-                                        onClick: () => navigate('/personal')
-                                    },
-                                    {
-                                        key: '3',
-                                        label: 'Salir',
-                                        icon: <AiOutlineLogout />,
-                                        onClick: () => showModal()
-                                    }
+                                {
+                                    key: '2',
+                                    label: 'Prestamos',
+                                    icon: <SwapOutlined />,
+                                    onClick: () => navigate('/prestamos')
+                                },
+                                {
+                                    key: '1',
+                                    label: 'Personal',
+                                    icon: <AiOutlineUserAdd/>,
+                                    onClick: () => navigate('/personal')
+                                },
+                                {
+                                    key: '3',
+                                    label: 'Salir',
+                                    icon: <AiOutlineLogout />,
+                                    onClick: () => showModal()
+                                }
                             ]}
                         }
                         trigger={['click']}
@@ -91,35 +95,35 @@ export default function FooterLayout({showModal, userPermission}) {
                 <Dropdown
                     className="fixed right-7 bottom-24 z-50"
                     menu={{
-                        items:[
-                                hasPermission(userPermission, 'crear vales') ?
-                                {
-                                    key: '5',
-                                    label: (
-                                    <Link className="dropDownResponsive" to={'/vales-salida/nuevo'}>
-                                        Registrar Vale
-                                    </Link>
-                                    ),
-                                }:
-                                null,
-                                hasPermission(userPermission, 'crear personal') ?
-                                {
-                                    key: '6',
-                                    label: (
-                                    <Link className="dropDownResponsive" to={'/personal/create'}>
-                                        Registrar Personal
-                                    </Link>
-                                    ),
-                                } : null,
-                                {
-                                    key: '7',
-                                    label: (
-                                    <Link className="dropDownResponsive" to={'/bitacora/form'}>
-                                        Registrar Bitacora
-                                    </Link>
-                                    ),
-                                }
-                            ]}
+                    items:[
+                        hasPermission(userPermission, 'crear vales') ?
+                        {
+                            key: '5',
+                            label: (
+                            <Link className="dropDownResponsive" to={'/vales-salida/nuevo'}>
+                                Registrar Vale
+                            </Link>
+                            ),
+                        }:
+                        null,
+                        hasPermission(userPermission, 'crear personal') ?
+                        {
+                            key: '6',
+                            label: (
+                            <Link className="dropDownResponsive" to={'/personal/create'}>
+                                Registrar Personal
+                            </Link>
+                            ),
+                        } : null,
+                        {
+                            key: '7',
+                            label: (
+                            <Link className="dropDownResponsive" to={'/bitacora/form'}>
+                                Registrar Bitacora
+                            </Link>
+                            ),
+                        }
+                        ]}
                     }
                     placement="topRight"
                     trigger={'click'}
@@ -131,8 +135,6 @@ export default function FooterLayout({showModal, userPermission}) {
                 </Dropdown>
                 : null 
             }
-            
-            
         </Footer>
     </>
     )
